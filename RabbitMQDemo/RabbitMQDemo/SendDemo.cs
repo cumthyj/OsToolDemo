c0d413a422b6a7332bfe2a482a3dc1d3eb74cdbe
cumthyj <cumthyj@outlook.com>
2018-07-18 15:24:14 +0800

    socket、Rabbit MQ demo

---
+++ RabbitMQDemo/RabbitMQDemo/SendDemo.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send
+{
+    class SendDemo
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var factory = new ConnectionFactory();
+                factory.HostName = "101.132.114.233";
+                factory.UserName = "hyj";
+                factory.Password = "123qwe";
+                factory.Port = 5672;
+
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        while (true)
+                        {
+                            channel.QueueDeclare("hello", false, false, false, null);
+                            Console.WriteLine("input string to send ：");
+                            object message = Console.ReadLine();
+                            var body = Encoding.UTF8.GetBytes(message.ToString());
+                            channel.BasicPublish("", "hello", null, body);
+                            Console.WriteLine(" send {0}", message);
+                        }
+                    }
+                }
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine(e1.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQDemo/Receive/ReceiveDemo.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receive
+{
+    class ReceiveDemo
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "101.132.114.233";
+            factory.UserName = "hyj";
+            factory.Password = "123qwe";
+            factory.Port = 5672;
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine(" waiting for message.");
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received {0}", message);
+
+                    }
+                }
+            }
+        }
+    }
+}
