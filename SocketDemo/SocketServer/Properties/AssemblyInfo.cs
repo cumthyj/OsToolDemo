fe1c2bf37ed3be3da005754618b6cb7a4db4f027
cumthyj <cumthyj@outlook.com>
2018-07-18 15:41:34 +0800

    socket changed

--- SocketDemo/SocketServer/Properties/AssemblyInfo.cs
+++ SocketDemo/SocketServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("SocketServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("SocketServer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("aaf3935f-bfeb-49da-bf73-baaf55a78e11")]
+[assembly: Guid("a9826640-06e8-4192-97fe-846ed4f2b45c")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
