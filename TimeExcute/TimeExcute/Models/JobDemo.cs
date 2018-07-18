0bee32d0bb2fbdbc33727114d593b61ee19b0ffe
cumthyj <cumthyj@outlook.com>
2018-07-18 16:11:38 +0800

    Interval Excute

---
+++ TimeExcute/TimeExcute/Models/JobDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using System.IO;
+
+namespace TimeExcute.Models
+{
+    public class JobDemo : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var reportDirectory = string.Format("~/reports/{0}/", DateTime.Now.ToString("yyyy-MM"));
+            reportDirectory = System.Web.Hosting.HostingEnvironment.MapPath(reportDirectory);
+            if (!Directory.Exists(reportDirectory))
+            {
+                Directory.CreateDirectory(reportDirectory);
+            }
+            var dailyReportFullPath = string.Format("{0}report_{1}.log", reportDirectory, DateTime.Now.Day);
+            var logContent = string.Format("{0}==>>{1}{2}", DateTime.Now, "create new log.", Environment.NewLine);
+            File.AppendAllText(dailyReportFullPath, logContent);
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ TimeExcute/TimeExcute/Models/TriggerDemo.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeExcute.Models
+{
+    public class SchedulerDemo
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<JobDemo>().Build();
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("triggerName", "groupName")
+              .WithSimpleSchedule(t =>
+                t.WithIntervalInSeconds(5)
+                 .RepeatForever())
+                 .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
